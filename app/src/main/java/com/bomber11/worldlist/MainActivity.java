ca596c75aee2b02ed184f6b8fc8bd5854c068eb5
bomber11 <anshul121998@gmail.com>
2020-04-28 00:15:32 +0530

    commiting

--- app/src/main/java/com/bomber11/worldlist/MainActivity.java
+++ app/src/main/java/com/bomber11/worldlist/MainActivity.java
@@ -1,14 +1,83 @@
 package com.bomber11.worldlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String urlJsonArry = "https://restcountries.eu/rest/v2/all";
+    Button button;
+    TextView textView;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btnArrayRequest);
+        textView = findViewById(R.id.txtResponse);
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+        Log.d("I am runnnign", "yes");
     }
+
+    public void loadJSON(View v) {
+        showpDialog();
+        JsonArrayRequest req = new JsonArrayRequest(urlJsonArry,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            String jsonResponse = "";
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject county = (JSONObject) response.get(i);
+
+                                String name = county.getString("name");
+                                String countyCode = county.getString("alpha2Code");
+                                jsonResponse += "Name: " + name + "\n";
+                                jsonResponse += "Country Code: " + countyCode + "\n-----------------\n";
+
+                            }
+                            textView.setText(jsonResponse);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        hidepDialog();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                hidepDialog();
+            }
+        });
+    }
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
 }
