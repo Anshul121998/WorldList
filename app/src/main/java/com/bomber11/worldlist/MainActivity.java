a8d2824a5cf8915951d15840421f986f7d26c9df
anshul jain <bomber11@anshuls-MacBook-Pro.local>
2020-04-27 22:09:15 +0530

    Initial commit

---
+++ app/src/main/java/com/bomber11/worldlist/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bomber11.worldlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
