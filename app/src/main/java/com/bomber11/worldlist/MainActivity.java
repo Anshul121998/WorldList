bcba11379fdeb8183424b67a97c42a7fcdee3b83
bomber11 <anshul121998@gmail.com>
2020-04-28 01:00:36 +0530

    commiting1

--- app/src/main/java/com/bomber11/worldlist/MainActivity.java
+++ app/src/main/java/com/bomber11/worldlist/MainActivity.java
@@ -11,9 +11,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,10 +26,10 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String urlJsonArry = "https://restcountries.eu/rest/v2/all";
     Button button;
     TextView textView;
     private ProgressDialog pDialog;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,39 +40,40 @@ public class MainActivity extends AppCompatActivity {
         pDialog = new ProgressDialog(this);
         pDialog.setMessage("Please wait...");
         pDialog.setCancelable(false);
+        requestQueue = Volley.newRequestQueue(this);
         Log.d("I am runnnign", "yes");
     }
 
     public void loadJSON(View v) {
         showpDialog();
-        JsonArrayRequest req = new JsonArrayRequest(urlJsonArry,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        try {
-                            String jsonResponse = "";
-                            for (int i = 0; i < response.length(); i++) {
-                                JSONObject county = (JSONObject) response.get(i);
-
-                                String name = county.getString("name");
-                                String countyCode = county.getString("alpha2Code");
-                                jsonResponse += "Name: " + name + "\n";
-                                jsonResponse += "Country Code: " + countyCode + "\n-----------------\n";
-
-                            }
-                            textView.setText(jsonResponse);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        hidepDialog();
-                    }
-                }, new Response.ErrorListener() {
+        String url = "https://restcountries.eu/rest/v2/all";
+        StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                textView.setText(response);
+                hidepDialog();
+//                try {
+//                    JSONObject jsonObject = new JSONObject(response);
+//                    JSONArray jsonArray = jsonObject.getJSONArray("data");
+//                    for (int i = 0; i < jsonArray.length(); i++) {
+//                        JSONObject jo = jsonArray.getJSONObject(i);
+//                        // Do you fancy stuff
+//                        // Example: String gifUrl = jo.getString("url");
+//                    }
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
+                // Anything you want
                 hidepDialog();
+                error.printStackTrace();
             }
         });
+        requestQueue.add(stringRequest);
+
     }
 
     private void showpDialog() {
